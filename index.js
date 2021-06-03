872981579b0fc3a0cc3b5611038f8a04f94638e9
gavin-rilee <you@example.com>
2021-06-03 13:57:09 -0400

    built react

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
 const server = express()
+
+server.use(express.json())
+server.use(cors())
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 
 console.log(process.env.LANG) //! secerts in env  LANG=en_US.UTF-8 
@@ -11,18 +18,25 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 const PORT = process.env.PORT || 5000
+
 console.log('port is on ->', PORT)
+
+server.get('/', (req,res) => {
+    //! sending back index.html
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+
 
 server.get('/api', (req, res) => {
     res.json({
-        message: `${process.env.COHORT} is the shit`
+        message: `${process.env.COHORT} is the best`
     })
 })
 
 server.use((req, res) => {
     res.status(404).json({
         message: `something is very wrong here`
-    })
+    })                                                                           
 })
 
 server.listen(PORT, () => {
