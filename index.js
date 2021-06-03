6a2a486f71c4754cac0f6f7e02f9bcc55b6ede35
gavin-rilee <you@example.com>
2021-06-03 13:15:38 -0400

    more changes

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('hey!!!!')
+require('dotenv').config()
+const express = require('express')
+const server = express()
+
+
+console.log(process.env.LANG) //! secerts in env  LANG=en_US.UTF-8 
+console.log(process.env.OS) //! OS=Windows_NT
+
+if(process.env.NODE_ENV === 'production'){
+    console.log('this means the code is deployed ')
+}
+
+const PORT = process.env.PORT || 5000
+console.log('port is on ->', PORT)
+
+server.get('/api', (req, res) => {
+    res.json({
+        message: `${process.env.COHORT} is the shit`
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
